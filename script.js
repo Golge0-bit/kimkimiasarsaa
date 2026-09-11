40bad4cf309eaf7e415c57dacb82928388bf9271
Golge0-bit <gokdenizzmumcu0@gmail.com>
2026-09-11 09:34:07 +0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -153,16 +153,16 @@ let mevcutPuan = 0;
 let bitti = false;
 
 // SES AYARLARI & MÜZİK (voise klasörü)
-const arkaPlanMuzigi = new Audio("voise/muzik.mp3");
+const arkaPlanMuzigi = new Audio("muzik.mp3");
 arkaPlanMuzigi.loop = true;
 
 const sesEfektleri = {
-  buton: new Audio("voise/buton.mp3"),
-  dogru: new Audio("voise/dogru.mp3"),
-  pas: new Audio("voise/pas.mp3"),
-  yanlis: new Audio("voise/yanlis.mp3"),
-  dogrutus: new Audio("voise/dogrutus.mp3"),
-  yanlistus: new Audio("voise/yanlistus.mp3")
+  buton: new Audio("buton.mp3"),
+  dogru: new Audio("dogru.mp3"),
+  pas: new Audio("pas.mp3"),
+  yanlis: new Audio("yanlis.mp3"),
+  dogrutus: new Audio("dogrutus.mp3"),
+  yanlistus: new Audio("yanlistus.mp3")
 };
 
 // Başlangıç Ses Seviyeleri (%5 Müzik, %50 Efekt)
